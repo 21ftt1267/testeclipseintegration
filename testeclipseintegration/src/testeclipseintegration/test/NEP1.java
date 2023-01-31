790d469bcac92fdad4783f273b8a0fa4a19a99db
Jade <21FTT1267@student.pb.edu.bn>
2023-01-31 15:27:53 +0800

    Trying basic OOP

--- testeclipseintegration/src/testeclipseintegration/test/NEP1.java
+++ testeclipseintegration/src/testeclipseintegration/test/NEP1.java
@@ -2,6 +2,8 @@ package testeclipseintegration.test;
 
 public class NEP1 {
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("Github");
+		classA classA= new classA("ABCDEF");
+		System.out.println(classA.getAbc());
 	}
 }
---
+++ testeclipseintegration/src/testeclipseintegration/test/classA.java
@@ -0,0 +1,20 @@
+package testeclipseintegration.test;
+
+public class classA {
+	
+	private String abc;
+	
+	public classA(String abc) {
+		this.abc = abc;
+		
+	}
+	
+	public String getAbc() {
+		return abc;
+	}
+	
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
+	
+}
